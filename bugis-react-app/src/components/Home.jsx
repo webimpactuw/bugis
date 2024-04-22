512551afc8415bd7bb925a711a12e39775440cdd
Samriddhi Sivakumar <73621618+SamriddhiS2@users.noreply.github.com>
2024-04-22 12:22:36 -0700

    Added About Bugis Section in the home page

--- bugis-react-app/src/components/Home.jsx
+++ bugis-react-app/src/components/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import days from '../assets/days-icon.png'
 import time from '../assets/time-icon.png'
 import location from '../assets/location-icon.png'
+import about from '../assets/about-bugis.png'
 
 const Home = () => {
   return (
@@ -14,36 +15,40 @@ const Home = () => {
           <p className='text-2xl tracking-wide'>MON – SAT</p>
         </li>
         <li className='flex space-x-4 items-center'>
-        <img className='h-10' src={time} alt="time-icon" />
+          <img className='h-10' src={time} alt="time-icon" />
           <p className='text-2xl tracking-wide'>11AM – 9PM</p>
         </li>
         <li className='flex space-x-4 items-center'>
-        <img className='h-10' src={location} alt="location-icon" />
+          <img className='h-10' src={location} alt="location-icon" />
           <p className='text-2xl tracking-wider underline decoration-[#E45B93] underline-offset-4'>UDistrict</p>
         </li>
       </ul>
 
       <div className='h-96 bg-[#FCECE7] pt-10 text-center'>
-        <button className='bg-[#E45B93] py-2.5 px-8 rounded-full'>
-          <p className='text-white text-2xl font-poppins font-semibold'>Explore Menu</p>
+        <button className='bg-[#E45B93] py-3 px-14 rounded-full'>
+          <p className='text-white text-xl font-poppins font-semibold tracking-wide'>Explore Menu</p>
         </button>
       </div>
 
-      <div className='pt-20'>
-        <h1>About BUGIS</h1>
+      <div className='flex justify-evenly pt-64 pb-72'>
+        <img className='h-auto max-w-lg pl-14' src={about} alt="about-bugis" />
 
-        <article>
-          <p>
-            We serve Asian, delicious, and hearty bowls for students to
-            <br />eat in a variety of ways at a reasonable price.
-            <br />
-            <br />Each order is crafted to perfection by the owner himself.
-          </p>
-        </article>
+        <div className='pt-10'>
+          <h1 className='font-librebaskerville text-6xl tracking-wide'>About BUGIS</h1>
 
-        <button className='bg-[#E45B93] py-2.5 px-8 rounded-full'>
-          <p className='text-white text-2xl font-poppins font-semibold'>Learn More</p>
-        </button>
+          <article className='font-poppins text-2xl pb-24 pt-10 tracking-wide font-light'>
+            <p>
+              We serve Asian, delicious, and hearty bowls for students to
+              <br />eat in a variety of ways at a reasonable price.
+              <br />
+              <br />Each order is crafted to perfection by the owner himself.
+            </p>
+          </article>
+
+          <button className='bg-[#E45B93] py-3 px-14 rounded-full'>
+            <p className='text-white text-xl font-poppins font-semibold tracking-wide'>Learn More</p>
+          </button>
+        </div>
       </div>
     </div>
   )
